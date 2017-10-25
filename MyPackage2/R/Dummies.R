10b15df30ac6e945817adfc6373693e5be8b09af
eloydelarubia <eloydelarubia@hotmail.com>
2017-10-25 19:58:57 +0200

    Update Dummies.R

--- MyPackage2/R/Dummies.R
+++ MyPackage2/R/Dummies.R
@@ -1,14 +1,5 @@
 
 
-# Instalamos el paquete devtools en el caso de ser necesario
-# install.packages("devtools")
-
-# Instalamos el paquete roxygen2 en el caso de ser necesario
-#install.packages("roxygen2")
-
-# Cargamos la librería devtools
-#library(devtools)
-
 # Creamos un par de funciones dummies
   # Cuadrado
   cuadrado <- function(x){
@@ -19,19 +10,3 @@
   resta <- function(a, b){
     return (a-b)
   }
-
-
-# Cargamos el paquete
-#load_all()
-
-# Subimos las funciones dummies al repositorio
-
-
-
-# Instalar el paquete
-# devtools::install_github("username/packagename")
-#
-#
-git init
-
-git remote add origin git@github.com:eloydelarubia/ProgramacionR
